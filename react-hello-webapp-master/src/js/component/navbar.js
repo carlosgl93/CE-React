d8e9b76add9d7272c8626ca2c3c217149675d1a9
kanogl <cgumucio93@gmail.com>
2020-07-15 09:16:51 -0400

    updated 15/07/2020

--- react-hello-webapp-master/src/js/component/navbar.js
+++ react-hello-webapp-master/src/js/component/navbar.js
@@ -1,17 +1,35 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
+import { logoText } from "../../img/logoText.png";
+import { logoImg } from "../../img/logoImg.png";
 
-export const Navbar = () => {
+export const Nav = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
+		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+			<Navbar.Brand href="#home">
+				<img
+					src={logoImg}
+					width="50"
+					height="50"
+					className="d-inline-block align-top"
+					alt="Cognitive English logo"
+				/>
+				<img
+					src={logoText}
+					width="150"
+					height="50"
+					className="d-inline-block align-top"
+					alt="Cognitive English logo"
+				/>
+			</Navbar.Brand>
+			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+			<Navbar.Collapse id="responsive-navbar-nav">
+				<Nav className="mr-auto">
+					<Nav.Link href="#features">Planes</Nav.Link>
+					<Nav.Link href="#pricing">Contacto</Nav.Link>
+					<Nav.Link href="#pricing">Corporativo</Nav.Link>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
 	);
 };
--- react-hello-webapp-master/src/js/layout.js
+++ react-hello-webapp-master/src/js/layout.js
@@ -3,11 +3,9 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
+import { Nav } from "./component/navbar";
 import { Footer } from "./component/footer";
 
 //create your first component
@@ -20,11 +18,10 @@ export const Layout = () => {
 		<div className="d-flex flex-column h-100">
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
-					<Navbar />
+					<Nav />
 					<Switch>
 						<Route exact path="/" component={Home} />
-						<Route path="/demo" component={Demo} />
-						<Route path="/single/:theid" component={Single} />
+
 						<Route render={() => <h1>Not found!</h1>} />
 					</Switch>
 					<Footer />
