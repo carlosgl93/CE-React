53f6b34cb6002f9403c516c6889513e79c138020
kanogl <cgumucio93@gmail.com>
2020-07-25 14:23:45 -0400

    error de renderizado

--- react-hello-webapp-master/src/js/layout.js
+++ react-hello-webapp-master/src/js/layout.js
@@ -1,34 +1,27 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop";
+import Home from "./views/home";
 
-import { Home } from "./views/home";
 import injectContext from "./store/appContext";
-
-import { Nav } from "./component/navbar";
+import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
-
 //create your first component
-export const Layout = () => {
+const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
-
 	return (
-		<div className="d-flex flex-column h-100">
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Nav />
-					<Switch>
-						<Route exact path="/" component={Home} />
-
-						<Route render={() => <h1>Not found!</h1>} />
-					</Switch>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
-		</div>
+		<BrowserRouter basename={basename}>
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route>
+					<h1>Not found!</h1>
+				</Route>
+			</Switch>
+			<Footer />
+		</BrowserRouter>
 	);
 };
-
 export default injectContext(Layout);
